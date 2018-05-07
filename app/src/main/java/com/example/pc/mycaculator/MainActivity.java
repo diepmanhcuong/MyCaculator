f0e6641fc8fe73be8fc35ab10828d50b9235bcd6
Cuongdm <diepmanhc0511@gmail.com>
2018-05-07 20:37:05 +0700

    lession_7_8

--- app/src/main/java/com/example/pc/mycaculator/MainActivity.java
+++ app/src/main/java/com/example/pc/mycaculator/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.pc.mycaculator;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private String display = "";
     private String currentOperator = "";
     private String result = "";
+    private final String LAST_RESULT = "LASTRESULT";
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         _screen = (TextView)findViewById(R.id.txresult);
-        _screen.setText(display);
+        sharedPreferences = getSharedPreferences(LAST_RESULT, Context.MODE_PRIVATE);
+        _screen.setText(sharedPreferences.getString("lastResult",""));
     }
 
     public void onClickNumber(View view) {
@@ -118,19 +127,49 @@ public class MainActivity extends AppCompatActivity {
         updateScreen();
     }
     public void onClickDoi(View view){
-        String resutlCurrent = _screen.getText().toString();
+        String resultCurrent = _screen.getText().toString();
         if(_screen.getText()!=null){
-            if(resutlCurrent.charAt(0)=='-'){
-                Double a = Double.parseDouble(resutlCurrent);
+            if(resultCurrent.charAt(0)=='-'){
+                Double a = Double.parseDouble(resultCurrent);
                 Double b = Math.abs(a);
                 String c = b.toString();
+
                 _screen.setText(c);
             }else {
-                _screen.setText("-"+resutlCurrent);
+                _screen.setText("-"+resultCurrent);
             }
 
         }
 
     }
 
+    public void save(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("lastResult",_screen.getText().toString());
+        editor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_operate, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    public void readData(){
+//        SharedPreferences  sharedPreferences = getSharedPreferences(LAST_RESULT,Context.MODE_PRIVATE);
+//        String
+//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.clear:
+                clear();
+                updateScreen();
+                break;
+            case R.id.save: save();
+            finish();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
